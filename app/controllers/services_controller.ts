4b6b1fdfdd200262658939c303479e1e868ec75b
fabienrak <DEV.fabmia@gmail.com>
2024-04-25 15:32:22 +0200

    chore: serviceController

---
+++ app/controllers/services_controller.ts
@@ -0,0 +1,47 @@
+import Service from '#models/service'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class ServicesController {
+
+    public async getServiceList({ response }: HttpContext){
+        try {
+            const service = await Service.all()
+            return response.ok({
+                "status":200,
+                "message":"SERVICE DISPONIBLE",
+                "data": {
+                    service
+                }
+            })
+        } catch(error){
+            return response.internalServerError({
+                "status":   500,
+                "message":  error
+            })
+        }
+    }
+
+    public async findOneService({params, response}: HttpContext){
+        try {
+            const oneService = await Service.find(params.id);
+            if(!oneService){
+                return response.notFound({
+                    "status": 404,
+                    "message":  "SERIVCE NON TROUVE"
+                })
+            }
+            return response.ok({
+                "status": 200,
+                "message":  "SERVICE TROUVER",
+                "data": {
+                    oneService
+                }
+            })
+        } catch(error){
+            return response.internalServerError({
+                "status": 500,
+                "message": error
+            })
+        }
+    }
+}
