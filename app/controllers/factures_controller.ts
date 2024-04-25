6a52f98fe8757e9c0ec44273ed2edfa4aebb223d
fabienrak <DEV.fabmia@gmail.com>
2024-04-25 15:35:39 +0200

    chore: Facture Controller

---
+++ app/controllers/factures_controller.ts
@@ -0,0 +1,69 @@
+import Facture from '#models/facture'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class FacturesController {
+
+    public async getFactureList({ response }: HttpContext){
+        try {
+            const factureList = await Facture.all()
+            return response.ok({
+                "status":200,
+                "message":"LISTE FACTURE DISPONIBLE",
+                "data": {
+                    factureList
+                }
+            })
+        } catch(error){
+            return response.internalServerError({
+                "status":   500,
+                "message":  error
+            })
+        }
+    }
+
+    public async deleteFacture({ params, response }: HttpContext){
+        try {
+            const factureEffacer: any = await Facture.find(params.id)
+            if(!factureEffacer){
+                return response.notFound({
+                    "status":   404,
+                    "message":  "FACTURE NON TROUVE",
+                })
+            }
+            await factureEffacer.delete()
+            return response.ok({
+                "status":   200,
+                "message":  "FACTURE BIEN SUPPRIMER"
+            })
+        } catch(error){
+            return response.internalServerError({
+                "status":   500,
+                "message":  error
+            })
+        }
+    }
+
+    public async findOneFacture({params, response}: HttpContext){
+        try {
+            const facture = await Facture.find(params.id);
+            if(!facture){
+                return response.notFound({
+                    "status": 404,
+                    "message":  "FACTURE NON TROUVE"
+                })
+            }
+            return response.ok({
+                "status": 200,
+                "message":  "FACTURE EXISTANT",
+                "data": {
+                    facture
+                }
+            })
+        } catch(error){
+            return response.internalServerError({
+                "status": 500,
+                "message": error
+            })
+        }
+    }
+}
