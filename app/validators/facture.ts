854f3456ced8385ffa42e2ed5e530aa1b68a5695
fabienrak <DEV.fabmia@gmail.com>
2024-04-25 16:46:28 +0200

    chore: facture validation

--- app/validators/facture.ts
+++ app/validators/facture.ts
@@ -1,13 +1,25 @@
 import vine from '@vinejs/vine'
 
-export const createArticleValidator = vine.compile(
+export const factureValidator = vine.compile(
     vine.object({
       reference_facture: vine.string().trim().minLength(8).unique(
         async (query, field)  => {
             const ref_facture = await query.from('factures').where('reference_facture', field).first()
             return !ref_facture
         }
-      )
+      ),
+      date_debut:   vine.date({
+            formats:   ['DD/MM/YYYY','x']
+        }
+      ),
+      date_fin: vine.date({
+            formats:   ['DD/MM/YYYY','x']
+        }
+      ),
+      montant_total: vine.number().decimal(2),
+      taxes:    vine.number().decimal(2)
     })
+
+    
   )
   
